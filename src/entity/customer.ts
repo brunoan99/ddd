cf6c6bd1ae68062e2a275ef73d28bfe75fbb4355
Bruno Andrade <brunoan99@gmail.com>
2022-09-28 23:55:48 -0300

    feat: add autovalidations to entity class

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -10,16 +10,36 @@ class Customer {
     this._name = name;
     this._address = address;
     this._active = true;
+    this.validate()
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error('Name is required')
+    }
+    if (this._id.length === 0) {
+      throw new Error('Id is required')
+    }
   }
   
   changeName(name: string) {
     if (name.split(' ').length <= 1) {
-      return new Error('Invalid name, names must contain at least first and last name.')
+      throw new Error('Invalid name, names must contain at least first and last name.')
     }
     this._name = name;
   }
 
   activate() {
+    if (this._name.length === 0) {
+      throw new Error('Name is required to activate a customer')
+    }
+    if (this._id.length === 0) {
+      throw new Error('Id is required to activate a customer')
+    }
+    if (this._address.length === 0) {
+      throw new Error('Address is required to activate a customer')
+    }
+
     this._active = true;
   }
 
