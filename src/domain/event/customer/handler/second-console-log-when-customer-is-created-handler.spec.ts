ba37264b23f39b093b79c73b572e4a1981cd72b1
Bruno Andrade <brunoan99@gmail.com>
2022-10-05 21:17:00 -0300

    test: ensure SecondConsoleLogWhenCustomerIsCreatedHandler console the correct message

---
+++ src/domain/event/customer/handler/second-console-log-when-customer-is-created-handler.spec.ts
@@ -0,0 +1,18 @@
+import { Customer } from "../../../entity/customer"
+import { EventDispatcher } from "../../@shared/event-dispatcher"
+import { CustomerCreatedEvent } from "../customer-created-event"
+import { SecondConsoleLogWhenCustomerIsCreatedHandler } from "./second-console-log-when-customer-is-created-handler"
+
+describe('Second ConsoleLog When Customer Is Created Handler', () => {
+  test('Should call console.log with correct data', () => {
+    const consoleSpy = jest.spyOn(console, 'log')
+    const sut = new SecondConsoleLogWhenCustomerIsCreatedHandler()
+    const eventDispatcher = new EventDispatcher()
+    eventDispatcher.register('CustomerCreatedEvent', sut)
+
+    const customer = new Customer("1", "Customer 1")
+    const event = new CustomerCreatedEvent(customer)
+    eventDispatcher.notify(event)
+    expect(consoleSpy).toHaveBeenCalledWith('Esse é o segundo console.log do evento: CustomerCreated')
+  })
+})
